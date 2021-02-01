39e40b17e0093b491a6240b8cefa72b8860515d6
Carol <castrocaroline11@gmail.com>
2021-02-01 05:42:55 -0300

    add validator interface

---
+++ src/backoffice/contracts/Customer.contracts.ts
@@ -0,0 +1,19 @@
+import { Validator } from "src/utils/validator";
+import { Customer } from "../models/customer.model";
+import { Contract } from "./Contract";
+
+export class CreateCustomerContract implements Contract {
+    errors: any[];
+    validate(model: Customer): boolean {
+       const validator = new Validator();
+
+       validator.hasMinLen(model.name, 5, "nome invalido" )
+       validator.isEmail(model.email, "Email invalido" )
+       validator.hasMinLen(model.name, 5, "Nome invalido" )
+
+       this.errors = validator.errors;
+
+       return validator.isValid();
+    }
+
+}
